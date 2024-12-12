979d7970318c27bf0ed998f27217cf57be6491e2
Long2Seth <long.piseth.1221@rupp.edu.kh>
2024-12-12 11:34:37 +0700

    update

--- src/lib/organizer/navData.ts
+++ src/lib/organizer/navData.ts
@@ -3,7 +3,7 @@ import { BellDot, Calendar, CreditCard, Database, FileText } from 'lucide-react'
 export const navItems = [
     { icon: Calendar, label: "EVENT" , link: "/organizer/events/detail-event"},
     { icon: CreditCard, label: "REVENUE | PAYMENT" ,link: "/organizer/payment&revenue"},
-    { icon: Database, label: "ORDER DATA" , link: "/organizer/order-data"},
+    { icon: Database, label: "ORDER DATA" , link: "/organizer/order"},
     { icon: FileText, label: "ATTENDANCE" , link: "/organizer/attendance"},
     { icon: BellDot, label: "NOTIFICATIONS", badge: "5" ,link: "/organizer/notifications" },
 ]
