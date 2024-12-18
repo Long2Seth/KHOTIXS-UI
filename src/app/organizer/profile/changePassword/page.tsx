32fd40a700440c16eda1ac8850e3a9fd37b65399
Long2Seth <long.piseth.1221@rupp.edu.kh>
2024-12-18 23:03:09 +0700

    update

--- src/app/organizer/profile/changePassword/page.tsx
+++ src/app/organizer/profile/changePassword/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 
 export default function ChangePassword (){
     return (
         <main className="p-6 space-y-6 max-w-[90rem] mx-auto">
-            <ChangePassword/>
+            {/*<ChangePassword/>*/}
         </main>
     );
 };
