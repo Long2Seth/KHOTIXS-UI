b5137f019021904c37795cac6f50a53f6fba6b77
Long2Seth <long.piseth.1221@rupp.edu.kh>
2024-12-19 09:24:53 +0700

    update

--- src/app/(customer)/user-profile/payment/page.tsx
+++
@@ -1,9 +0,0 @@
-import Payment from "@/components/customer/User-Profile/Payment";
-
-export default function Payments(){
-    return (
-        <main>
-            <Payment/>
-        </main>
-    )
-}
