0b374d0d2306d3ddf69dc3e9ac8c46fa39d62411
SEAKNGIM <seakngimsr37@gmail.com>
2024-12-31 16:49:39 +0700

    update feedback process payment and contact us

--- src/components/customer/contact/ContactUsComponent.tsx
+++ src/components/customer/contact/ContactUsComponent.tsx
@@ -6,10 +6,65 @@ import {GrLocation} from "react-icons/gr";
 import {FiPhone} from "react-icons/fi";
 import {HiOutlineMail} from "react-icons/hi";
 import {Input} from "@/components/ui/input";
+import Link from "next/link";
 
+type FormErrors = {
+    name: string;
+    email: string;
+    message: string;
+};
 
 export default function ContactUsComponent() {
     const [isLoading, setIsLoading] = useState(true);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errors, setErrors] = useState<FormErrors>({name: '', email: '', message: ''});
+
+
+    const validateForm = () => {
+        const formErrors = {name: '', email: '', message: ''};
+        let isValid = true;
+
+        if (!name) {
+            formErrors.name = 'Name is required';
+            isValid = false;
+        }
+        if (!email) {
+            formErrors.email = 'Email is required';
+            isValid = false;
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            formErrors.email = 'Email is invalid';
+            isValid = false;
+        }
+        if (!message) {
+            formErrors.message = 'Message is required';
+            isValid = false;
+        }
+
+        setErrors(formErrors);
+        return isValid;
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (validateForm()) {
+            // Handle form submission
+            console.log('Form submitted');
+        }
+    };
+
+    const handleBlur = (field: keyof FormErrors, value: string) => {
+        const newErrors: FormErrors = {...errors};
+        if (value.trim() === '') {
+            newErrors[field] = `${field.charAt(0).toUpperCase() + field.slice(1)} is required`;
+        } else if (field === 'email' && !/\S+@\S+\.\S+/.test(value)) {
+            newErrors.email = 'Email is invalid';
+        } else {
+            newErrors[field] = '';
+        }
+        setErrors(newErrors);
+    };
     useEffect(() => {
         const timer = setTimeout(() => {
             setIsLoading(false);
@@ -20,7 +75,8 @@ export default function ContactUsComponent() {
         <>
             {
                 isLoading ? <ContactSkeletonComponent/> :
-                    <section className=" flex flex-col space-y-[50px] md:space-y-[80px] xl:space-y-[100px] mb-[50px] md:mb-[80px] xl:mb-[100px] bg-khotixs-background-white dark:bg-khotixs-background-dark ">
+                    <section
+                        className=" flex flex-col space-y-[50px] md:space-y-[80px] xl:space-y-[100px] mb-[50px] md:mb-[80px] xl:mb-[100px] bg-khotixs-background-white dark:bg-khotixs-background-dark ">
                         {/* Hero section */}
                         <section className="relative h-[400px] w-full overflow-x-hidden xl:h-[600px]">
                             <div
@@ -40,7 +96,7 @@ export default function ContactUsComponent() {
                         {/* Contact Info and Form */}
                         <section
                             className="container mx-auto w-full h-auto text-primary-color-text dark:text-secondary-color-text">
-                            <div className="grid grid-cols-1 gap-[100px] w-full md:grid-cols-2 px-[24px]">
+                            <div className="grid grid-cols-1 lg:gap-[100px] gap-5 w-full md:grid-cols-2 px-[24px]">
                                 {/* Left Section: Contact Information */}
                                 <div className="flex flex-col gap-8 text-center">
                                     <h1 className="text-title-color text-lg md:text-2xl xl:text-4xl font-bold uppercase w-full text-center dark:text-white">
@@ -52,10 +108,12 @@ export default function ContactUsComponent() {
                                         out to us by filling out the contact form below. We&apos;ll be in touch shortly.
                                     </p>
                                     <div className="flex flex-col gap-5 text-lg">
-                                        <div
-                                            className="flex items-center gap-4 bg-white px-5 py-2 rounded-[5px] dark:bg-khotixs-background-dark dark:border dark:border-secondary-color-text">
-                                            <GrLocation className="w-[30px] h-[30px]"/>
-                                            <div className="text-start">
+                                        <Link href="https://maps.app.goo.gl/V9rV5hL13QNK3ZJg9"
+                                              className="grid grid-cols-12 hover:opacity-80 items-center bg-white px-5 py-2 rounded-[5px] dark:bg-background-blur dark:bg-opacity-5">
+                                            <div className="col-span-2">
+                                                <GrLocation className="w-[30px] h-[30px] text-primary-color"/>
+                                            </div>
+                                            <div className="text-start col-span-10">
                                                 <h3 className="text-title-color text-base md:text-lg xl:text-xl font-bold dark:text-secondary-color-text">
                                                     Location
                                                 </h3>
@@ -63,28 +121,32 @@ export default function ContactUsComponent() {
                                                     No. 24, St 562, Sangkat Boeung Kak I, Khan Toul Kork, Phnom Penh.
                                                 </p>
                                             </div>
-                                        </div>
-                                        <div
-                                            className="flex items-center gap-4 bg-white px-5 py-2 rounded-[5px] dark:bg-khotixs-background-dark dark:border dark:border-secondary-color-text">
-                                            <FiPhone className="w-[30px] h-[30px]"/>
-                                            <div className="text-start">
+                                        </Link>
+                                        <Link href="tel:+85595990910"
+                                              className="grid grid-cols-12 hover:opacity-80 items-center bg-white px-5 py-2 rounded-[5px] dark:bg-background-blur dark:bg-opacity-5">
+                                            <div className="col-span-2">
+                                                <FiPhone className="w-[30px] h-[30px] text-primary-color"/>
+                                            </div>
+                                            <div className="text-start col-span-10">
                                                 <h3 className="text-title-color text-base md:text-lg xl:text-xl font-bold dark:text-secondary-color-text">
                                                     Phone
                                                 </h3>
                                                 <p className="text-description-color text-sm md:text-base xl:text-lg dark:text-dark-description-color">(+855)
                                                     95 990 910</p>
                                             </div>
-                                        </div>
-                                        <div
-                                            className="flex items-center gap-4 bg-white px-5 py-2 rounded-[5px] dark:bg-khotixs-background-dark dark:border dark:border-secondary-color-text">
-                                            <HiOutlineMail className="w-[30px] h-[30px]"/>
-                                            <div className="text-start">
+                                        </Link>
+                                        <Link href="https://mail.google.com/mail/u/0/#sent?compose=new"
+                                              className="grid grid-cols-12 hover:opacity-80 items-center bg-white px-5 py-2 rounded-[5px] dark:bg-background-blur dark:bg-opacity-5">
+                                            <div className="col-span-2">
+                                                <HiOutlineMail className="w-[30px] h-[30px] text-primary-color"/>
+                                            </div>
+                                            <div className="text-start col-span-10">
                                                 <h3 className="text-title-color text-base md:text-lg xl:text-xl font-bold dark:text-secondary-color-text">
                                                     Email
                                                 </h3>
                                                 <p className="text-description-color text-sm md:text-base xl:text-lg dark:text-dark-description-color">inform.service@gmail.com</p>
                                             </div>
-                                        </div>
+                                        </Link>
                                     </div>
                                 </div>
 
@@ -99,9 +161,9 @@ export default function ContactUsComponent() {
                                         our best to respond promptly.
                                     </p>
                                     <div
-                                        className="p-5 xl:p-20 text-start rounded-[5px] bg-white dark:bg-khotixs-background-dark dark:border-[1px] dark:border-white">
-                                        <form className="flex flex-col gap-4">
-                                            <div className="flex flex-col">
+                                        className="p-5 xl:p-20 text-start rounded-[5px] bg-white dark:bg-background-blur dark:bg-opacity-5">
+                                        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                                            <div className="flex flex-col gap-2">
                                                 <label
                                                     htmlFor="name"
                                                     className="text-base font-medium text-primary-color-text dark:text-secondary-color-text">
@@ -111,10 +173,15 @@ export default function ContactUsComponent() {
                                                     type="text"
                                                     id="name"
                                                     placeholder="Name"
-                                                    className="p-2 text-lg border-gray-300 rounded-[6px] dark:border placeholder:text-gray-300 dark:border-white dark:text-secondary-color-text dark:bg-khotixs-background-dark"
+                                                    value={name}
+                                                    onChange={(e) => setName(e.target.value)}
+                                                    onBlur={(e) => handleBlur('name', e.target.value)}
+                                                    className="p-2 text-lg border-gray-300 rounded-[6px] dark:border-label-description placeholder:text-gray-300
+                                                     dark:text-secondary-color-text dark:bg-background-blur dark:bg-opacity-5"
                                                 />
+                                                {errors.name && <span className="text-red-500">{errors.name}</span>}
                                             </div>
-                                            <div className="flex flex-col">
+                                            <div className="flex flex-col gap-2">
                                                 <label htmlFor="email"
                                                        className="text-lg font-medium text-primary-color-text dark:text-secondary-color-text">
                                                     Email <span className="text-red-500">*</span>
@@ -123,10 +190,15 @@ export default function ContactUsComponent() {
                                                     type="email"
                                                     id="email"
                                                     placeholder="Email"
-                                                    className="p-2 text-lg border-gray-300 rounded-[6px] dark:border placeholder:text-gray-300 dark:border-white dark:text-secondary-color-text dark:bg-khotixs-background-dark"
+                                                    value={email}
+                                                    onChange={(e) => setEmail(e.target.value)}
+                                                    onBlur={(e) => handleBlur('email', e.target.value)}
+                                                    className="p-2 text-lg border-gray-300 rounded-[6px] dark:border-label-description placeholder:text-gray-300
+                                                     dark:text-secondary-color-text dark:bg-background-blur dark:bg-opacity-5"
                                                 />
+                                                {errors.email && <span className="text-red-500">{errors.email}</span>}
                                             </div>
-                                            <div className="flex flex-col">
+                                            <div className="flex flex-col gap-2">
                                                 <label htmlFor="message"
                                                        className="text-lg font-medium text-primary-color-text dark:text-secondary-color-text">
                                                     Message <span className="text-red-500">*</span>
@@ -134,12 +206,18 @@ export default function ContactUsComponent() {
                                                 <textarea
                                                     id="message"
                                                     placeholder="Message"
-                                                    className="p-2 text-lg border border-gray-300 rounded-[6px] dark:border placeholder:text-gray-300 dark:border-white dark:text-secondary-color-text dark:bg-khotixs-background-dark"
+                                                    value={message}
+                                                    onChange={(e) => setMessage(e.target.value)}
+                                                    onBlur={(e) => handleBlur('message', e.target.value)}
+                                                    className="p-2 text-lg border border-gray-300 rounded-[6px] dark:border-label-description placeholder:text-gray-300
+                                                     dark:text-secondary-color-text bg-transparent"
                                                 ></textarea>
+                                                {errors.message &&
+                                                    <span className="text-red-500">{errors.message}</span>}
                                             </div>
                                             <button
                                                 type="submit"
-                                                className="bg-primary-color text-white text-lg py-2 px-4 rounded-[5px] hover:bg-primary-color-dark transition"
+                                                className="bg-primary-color text-white text-lg py-2 px-4 rounded-[5px] hover:bg-primary-color/80 transition"
                                             >
                                                 Submit
                                             </button>
@@ -155,7 +233,7 @@ export default function ContactUsComponent() {
                             <h1 className="text-title-color text-lg md:text-2xl xl:text-4xl font-bold uppercase w-[300px] text-center mx-auto dark:text-white">
                                 OUR LOCATION
                             </h1>
-                            <div className="m-5">
+                            <Link href="https://maps.app.goo.gl/V9rV5hL13QNK3ZJg9" className="m-5">
                                 <iframe
                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3908.6314046843904!2d104.89920651123596!3d11.578259843843679!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310951e96d257a6f%3A0x6b66703c5fc0c7cc!2sScience%20and%20Technology%20Advanced%20Development%20Co.%2C%20Ltd.!5e0!3m2!1sen!2sus!4v1733450750727!5m2!1sen!2sus"
                                     style={{border: 0, height: '600px', width: '100%'}}
@@ -163,7 +241,7 @@ export default function ContactUsComponent() {
                                     loading="lazy"
                                     referrerPolicy="no-referrer-when-downgrade"
                                 ></iframe>
-                            </div>
+                            </Link>
                         </section>
                     </section>
             }
