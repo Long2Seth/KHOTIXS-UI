0ce4bee45f8e5701539caebf4e11849095061fc1
ProeungChiso <proeung.chiso.2821@rupp.edu.kh>
2025-01-03 10:49:13 +0700

    install dependencie

--- src/components/customer/card/InfoCard.tsx
+++ src/components/customer/card/InfoCard.tsx
@@ -8,9 +8,6 @@ export default function InfoCard({title}: InfoCardProps) {
             <h1 className=" text-title-color text-lg md:text-2xl xl:text-4xl font-bold dark:text-secondary-color-text ">
                 {title}
             </h1>
-            <p className=" text-description-color text-base md:text-lg xl:text-xl max-w-[500px] lg:max-w-[800px] sm:px-0 md:px-0 md:text-md lg:text-lgnfont-light text-center dark:text-dark-description-color">
-                {description}
-            </p>
         </section>
     )
 }
