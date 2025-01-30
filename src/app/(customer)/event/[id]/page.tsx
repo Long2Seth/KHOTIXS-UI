0ee13c32cbfa3f88b40693cd04080d0487937d19
Long2Seth <long.piseth.1221@rupp.edu.kh>
2025-01-30 15:31:23 +0700

    update metadata

--- src/app/(customer)/event/[id]/page.tsx
+++ src/app/(customer)/event/[id]/page.tsx
@@ -2,24 +2,24 @@ import EventDetails from "@/components/customer/event/EventDetail";
 import type {Metadata, ResolvingMetadata} from "next";
 import {EventType} from "@/lib/types/customer/event";
 
+const baseUrl = "http://localhost:8001"
 
 type Props = {
     params: Promise<{ id: string }>
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
-
 async function getEventMetadata(id: string): Promise<EventType | null> {
     try {
-        const res = await fetch(`/event-ticket/api/v1/events/${id}`);
-        if (!res.ok) return null;
-        return res.json();
+        const res = await fetch(`https://khotixs.istad.co/event-ticket/api/v1/events/${id}`);
+        const data = await res.json();
+        console.log("EVENT DATA: ", data);
+        return data;
     } catch (error) {
         console.error("Error fetching event metadata:", error);
         return null;
     }
 }
-
 
 export async function generateMetadata(
     {params}: Props,
@@ -27,13 +27,20 @@ export async function generateMetadata(
 ): Promise<Metadata> {
 
     const {id} = await params;
+    console.log("EVENT ID MMMM : ", id);
     const event = await getEventMetadata(id);
+
+    console.log(" EVENT METADATA : ", event);
 
     if (!event) {
         return {
             title: "Event Not Found",
         };
     }
+
+    console.log("  EVENT TITLE: ", event.eventTitle);
+    console.log("  EVENT DESCRIPTION: ", event.description);
+    console.log("  EVENT THUMBNAIL: ", event.thumbnail);
 
     const previousImages = (await parent).openGraph?.images || [];
 
@@ -48,8 +55,8 @@ export async function generateMetadata(
     };
 }
 
-
 export default async function EventPage({params}: Props) {
     const {id} = await params;
+    console.log("EVENT ID: ", id);
     return <EventDetails id={id}/>;
 }
