4e0456cc4c35ee97108435d50701f8cdff79d6a9
Long2Seth <long.piseth.1221@rupp.edu.kh>
2025-01-30 16:03:26 +0700

    update metadata

--- src/app/(customer)/layout.tsx
+++ src/app/(customer)/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import '../globals.css';
 import NavbarComponent from "@/components/customer/navbar/NavbarComponent";
 import { ThemeProvider } from "next-themes";
@@ -6,47 +5,10 @@ import { FooterComponent } from "@/components/customer/footer/FooterComponent";
 import { Toaster } from "@/components/ui/toaster"
 import ScrollToTopButton from "@/components/customer/home/ScrollToTopButton";
 import StoreProvider from "@/app/StoreProvider";
-import { ThemeWrapper } from "@/components/ThemeWrapper";
 import { TicketProvider } from "@/context/TicketContext";
 import { UserProvider } from "@/context/UserContext";
 import { QRProvider } from "@/context/QRContext";
 
-export const metadata: Metadata = {
-    metadataBase: new URL("https://khotixs.istad.co/"),
-    manifest: "/manifest.json",
-    title: {
-        template: "%s - KHOTIXS",
-        default: "KHOTIXS - Order your tickets, boost your sales, and experience the future of event management with KHOTIXS!",
-    },
-    description: "Welcome to KHOTIXS, the best place to buy and sell tickets for events online.",
-    keywords: ["event tickets", "buy tickets online", "sell tickets", "event organizers", "ticket buyers"],
-    category: 'Ticket Booking, Event Management, Technology',
-    openGraph: {
-        title: {
-            template: "%s - KHOTIXS",
-            default: "KHOTIXS - Order your tickets, boost your sales, and experience the future of event management with KHOTIXS!",
-        },
-        description: "Welcome to KHOTIXS, the best place to buy and sell tickets for events online.",
-        siteName: 'KHOTIXS',
-        images: [
-            {
-                url: 'https://khotixs.istad.co/asset/api/v1/files/view/37e026de-841f-40b6-a05d-d8254aa1057e.png',
-                width: 494,
-                height: 505,
-            },
-        ],
-    },
-    twitter: {
-        card: 'summary_large_image',
-        title: {
-            template: "%s - KHOTIXS",
-            default: "KHOTIXS - Order your tickets, boost your sales, and experience the future of event management with KHOTIXS!",
-        },
-        description: "Welcome to KHOTIXS, the best place to buy and sell tickets for events online.",
-        creator: '@KHOTIXS',
-        images: 'https://khotixs.istad.co/asset/api/v1/files/view/37e026de-841f-40b6-a05d-d8254aa1057e.png',
-    },
-};
 
 export default function RootLayout({
     children,
