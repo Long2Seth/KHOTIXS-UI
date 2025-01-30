ad3f749d46fff9ff6d99241caf03b24dbc7a4d8c
Long2Seth <long.piseth.1221@rupp.edu.kh>
2025-01-30 15:50:13 +0700

    update metadata

--- src/app/(customer)/event/[id]/page.tsx
+++ src/app/(customer)/event/[id]/page.tsx
@@ -2,7 +2,7 @@ import EventDetails from "@/components/customer/event/EventDetail";
 import type {Metadata, ResolvingMetadata} from "next";
 import {EventType} from "@/lib/types/customer/event";
 
-const baseUrl = "http://localhost:8001"
+
 
 type Props = {
     params: Promise<{ id: string }>
@@ -13,7 +13,6 @@ async function getEventMetadata(id: string): Promise<EventType | null> {
     try {
         const res = await fetch(`https://khotixs.istad.co/event-ticket/api/v1/events/${id}`);
         const data = await res.json();
-        console.log("EVENT DATA: ", data);
         return data;
     } catch (error) {
         console.error("Error fetching event metadata:", error);
@@ -27,20 +26,13 @@ export async function generateMetadata(
 ): Promise<Metadata> {
 
     const {id} = await params;
-    console.log("EVENT ID MMMM : ", id);
     const event = await getEventMetadata(id);
-
-    console.log(" EVENT METADATA : ", event);
 
     if (!event) {
         return {
             title: "Event Not Found",
         };
     }
-
-    console.log("  EVENT TITLE: ", event.eventTitle);
-    console.log("  EVENT DESCRIPTION: ", event.description);
-    console.log("  EVENT THUMBNAIL: ", event.thumbnail);
 
     const previousImages = (await parent).openGraph?.images || [];
 
